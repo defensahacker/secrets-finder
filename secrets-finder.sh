cbb7ab97893d78b38c75b79e03ffdb59c68e25e4
spinfoo <jacobo@defensahacker.com>
2019-09-11 13:21:44 +0200

    Version 0.1

---
+++ secrets-finder.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# -*- Secrets finder -*-
+#
+# Searches for secrets (passwords, private keys, tokens, ip addresses, etc)
+# inside any folder that contains source code
+#
+# 2019 Jacobo Avariento
+
+
+if [ $# -lt 1 ] ; then
+	echo "Usage: $0 <DIR1> (<DIR2> <DIR3> ...)"
+	exit
+fi
+
+for dir in $* ; do
+	egrep --color -nri "(password|login|token|credentials|username|https?://[^w])" $dir
+done
